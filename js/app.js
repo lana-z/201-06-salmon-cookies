02ac5f7e9976e929c8e637305b5b320fe4149650
lana-z <lana@levelupeconomy.com>
2023-10-31 17:11:30 -0700

    css improvements and some js added from class today but not ready yet

---
+++ js/app.js
@@ -0,0 +1,145 @@
+const rootElement = document.getElementById("root")
+console.log(rootElement)
+
+const Seattle = {
+    city: "Seattle",
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookie: 6.3,
+    phone: 111-111-1111
+
+}
+constSeattle = document.createEl
+
+
+
+
+let hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm"]
+
+
+
+    getRandomNumOfCustomers: function() {
+      return Math.floor( Math.random() * (max-min+1)) + min;
+    }
+  };
+
+function estimate(store) {
+  let estimate(store) = [];
+  for (let i=0; i < hours.length; i++)
+    const numCustomers = random( store.minCustomersPerHour, store.)//something here
+    const hourlySales = Math.ceil(numCustomers * store.averageCookies);//here
+    estimatedSales.push( hourlySales );
+}
+  return estimatedSales;
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Seattle = {
+//     city: "Seattle",
+//     minCustomer: 23,
+//     maxCustomer: 65,
+//     avgCookie: 6.3,
+       
+
+
+//     getRandomNumOfCustomers: function() {
+//       return Math.floor( Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer );
+//     }
+
+//     hourlyCookies: function() {
+//       const cookiesPerHour = [];
+//       for (let hour = 6; hour < 19; hour++) {
+//           const customers = this.getRandomNumOfCustomers();
+//           const cookiesSold = (customers * this.avgCookie);
+//           cookiesPerHour.push(cookiesSold);
+//       }
+//       return cookiesPerHour;
+//   }
+
+// };
+
+// let customersSeattle = Seattle.getRandomNumOfCustomers();
+// console.log(`Random number of customers in ${Seattle.city}: ${customersSeattle}`);
+
+
+
+//   const Tokyo = {
+//     city: "Tokyo",
+//     minCustomer: 3,
+//     maxCustomer: 24,
+//     avgCookie: 1.2,
+
+//     getRandomNumOfCustomers: function() {
+//       return Math.floor( Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer );
+//     }
+//   };
+
+// let customersTokyo = Tokyo.getRandomNumOfCustomers();
+// console.log(`Random number of customers in ${Tokyo.city}: ${customersTokyo}`);
+
+
+//   const Dubai = {
+//     city: "Dubai",
+//     minCustomer: 11,
+//     maxCustomer: 38,
+//     avgCookie: 3.7,
+
+//     getRandomNumOfCustomers: function() {
+//       return Math.floor( Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer );
+//     }
+//   };
+//   let customersDubai = Dubai.getRandomNumOfCustomers();
+//   console.log(`Random number of customers in ${Dubai.city}: ${customersDubai}`);
+  
+
+
+// const Paris = {
+//     city: "Paris",
+//     minCustomer: 20, 
+//     maxCustomer: 38,
+//     avgCookie: 2.3,
+
+//     getRandomNumOfCustomers: function() {
+//       return Math.floor( Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer );
+//     }
+// };
+// let customersParis = Paris.getRandomNumOfCustomers();
+// console.log(`Random number of customers in ${Paris.city}: ${customersParis}`);
+
+
+// const Lima = {
+//     city: "Lima",
+//     minCustomer: 2, 
+//     maxCustomer: 16,
+//     avgCookie: 4.6,
+
+//     getRandomNumOfCustomers: function() {
+//       return Math.floor( Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer );
+//     }
+// };
+// let customersLima = Lima.getRandomNumOfCustomers();
+// console.log(`Random number of customers in ${Lima.city}: ${customersLima}`);
+
+// function hourlyCookies
+
